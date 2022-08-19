978527649a2572e67ddd4deebc068ce85883e353
luisrohas <luisrodrigo@yahoo.com>
2022-08-19 15:29:44 -0500

    Archivos ok

---
+++ pwdchange.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MiNa</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    
+</head>
+<body>
+    <div class="contenedor contenido-header">
+            <div class="entrada">
+                <form action="mailpwd.php" method="POST" autocomplete="off">
+                    <label>Correo</label><input type="email" name="usuario">
+                    
+                    <input class="boton boton-verde" type="submit" name="enviar" value="Validar">
+                   
+                </form>
+                
+            </div>
+            <div class="home" style="margin-left:75px;">
+            <a href="/">
+        <img src="img/home2.jpg" alt="Home" width="5%" height="5%">
+        </a>
+        </div>
+        </div> 
+        
+</body>
+</html>
